f801c2347620d56441598ccd03cfd51bb11f3e29
MatthiasD25 <130077731+MatthiasD25@users.noreply.github.com>
2024-10-05 14:16:00 -0700

    Add files via upload

---
+++ Paper LISA/SMBHBpy/merger_system.py
@@ -0,0 +1,225 @@
+import numpy as np
+import SMBHBpy.cosmo as cosmo
+import SMBHBpy.halo as halo
+import SMBHBpy.constants as c
+
+
+class SystemProp:
+    """
+    A class describing the properties of a binary system
+
+    Attributes:
+        m1 (float): The mass of the first component - in units of (solar_mass *G/c**2)
+        m2 (float): The mass of the second component - in units of (solar_mass *G/c**2)
+        D  (float): The luminosity distance to the binary
+        halo1 (DMHalo): The object describing the dark matter halo around m1
+        halo2 (DMHalo): The object describing the dark matter halo around m2
+        Coulomb_log (float): Coulomb logarithm
+        sigma (float): velocity dispersion
+    """
+
+
+    def __init__(self, m1, m2, halo1, halo2, sigma, Coulomb_log, k=0, D=1., inclination_angle = 0., pericenter_angle=0., includeHaloInTotalMass=False, relcorr = False):
+        """
+        The constructor for the SystemProp class
+
+        Parameters:
+            m1 : float
+                The mass of the first component
+            m2 : float
+                The mass of the second component
+            halo1 (DMHalo): 
+                The object describing the dark matter halo around m1
+            halo2 (DMHalo): 
+                The object describing the dark matter halo around m2
+            D :  float
+                The luminosity distance to the binary
+            Coulomb_log : float
+                Coulomb logarithm
+            sigma : float
+                velocity dispersion
+            k : float
+                Indicates whether the halos are static (k = 0) or non-static, i.e. rotating (k = 1).
+                k = 0 can be selected independently of the eccentricity, k = 1 only if circular orbits are involved.
+            inclination_angle : float
+                The inclination angle (usually denoted iota) at which the system is oriented, see https://arxiv.org/pdf/1807.07163.pdf
+            pericenter_angle : float
+                The angle at which the pericenter is located wrt the observer, denoted as beta in https://arxiv.org/pdf/1807.07163.pdf
+            includeHaloInTotalMass : bool
+                Whether to include the dark matter halo mass in the calculation of the enclosed mass
+            relcorr : bool
+                Specifies whether relativistic effects should be considered in the calculation of dynamic friction or not.
+                https://arxiv.org/pdf/2204.12508.pdf (except for the typo in the gamma factor)
+        """
+        self.m1 = m1
+        self.m2 = m2
+
+        self.sigma = sigma
+        self.Coulomb_log = Coulomb_log
+        self.k = k
+        self.D = D
+
+        self.halo1 = halo1
+        self.halo2 = halo2
+        
+        self.halo1.r_min = self.r_isco_1() if halo1.r_min == 0. else halo1.r_min
+        self.halo2.r_min = self.r_isco_2() if halo2.r_min == 0. else halo2.r_min
+
+        self.inclination_angle = inclination_angle
+        self.pericenter_angle = pericenter_angle
+
+        self.includeHaloInTotalMass = includeHaloInTotalMass
+        self.relcorr = relcorr
+
+
+    def r_isco_1(self):
+        """
+        The function returns the radius of the Innermost Stable Circular Orbit (ISCO) of the Schwarzschild black hole with mass m1
+
+        Returns:
+            out : float
+                The radius of the ISCO of m1
+        """
+        return 6.*self.m1
+    
+    def r_isco_2(self):
+        """
+        The function returns the radius of the Innermost Stable Circular Orbit (ISCO) of the Schwarzschild black hole with mass m2
+
+        Returns:
+            out : float
+                The radius of the ISCO of m2
+        """
+        return 6.*self.m2
+    
+    def r_infl(self): # Also known as r_h
+        """
+        The function returns the (gravitational) influence radius of the binary with mass M = m1+m2
+
+        Returns:
+            out : float
+                The (gravitational) influence radius
+        """
+        return self.m_total()/(self.sigma)**2
+
+    def r_schwarzschild_1(self):
+        """
+        The function returns the Schwarzschild radius of the Schwarzschild black hole with mass m1
+
+        Returns:
+            out : float
+                The Schwarzschild radius of m1
+        """
+        return 2.*self.m1
+    
+    def r_schwarzschild_2(self):
+        """
+        The function returns the Schwarzschild radius of the Schwarzschild black hole with mass m1
+
+        Returns:
+            out : float
+                The Schwarzschild radius of m2
+        """
+        return 2.*self.m2
+
+    def m_reduced(self):
+        """
+        The function returns the reduced mass of the binary system of m1 and m2
+
+        Parameters:
+            r : float or array_like
+                The radius at which to evaluate the mass
+
+        Returns:
+            out : float
+                The reduced mass
+        """
+        return self.m1 * self.m2 / (self.m1 + self.m2)
+
+    def redshifted_m_reduced(self):
+        """
+        The function returns the redshifted reduced mass of the binary system of m1 and m2
+
+        Parameters:
+            r : float or array_like
+                The radius at which to evaluate the mass
+
+        Returns:
+            out : float
+                The redshifted reduced mass
+        """
+        return (1. + self.z()) * self.m_reduced()
+
+    def m_total(self):
+        """
+        The function returns the total mass of the binary system of m1 and m2
+
+        Parameters:
+            r : float or array_like
+                The radius at which to evaluate the mass
+        Returns:
+            out : float
+                The total mass
+        """
+        return self.m1 + self.m2
+
+    def m_chirp(self):
+        """
+        The function returns the chirp mass of the binary system of m1 and m2
+
+        Returns:
+            out : float
+                The chirp mass
+        """
+        return self.m_reduced()**(3./5.) * self.m_total()**(2./5.)
+
+    def redshifted_m_chirp(self):
+        """
+        The function returns the redshifted chirp mass of the binary system of m1 and m2
+
+        Returns:
+            out : float
+                The redshifted chirp mass
+        """
+        return (1.+self.z()) * self.m_chirp()
+
+    def z(self):
+        """
+        The function returns the redshift as a measure of distance to the system
+        According to the Hubble Law
+
+        Returns:
+            out : float
+                The redshift of the system
+        """
+        return cosmo.HubbleLaw(self.D)
+    
+    def omega_s(self, r):
+        """
+        The function returns the angular frequency of the binary in a circular orbit 
+        
+        Parameters:
+            r : float or array_like
+                The radius at which to evaluate the orbital frequency
+
+        Returns:
+            out : float or array_like (depending on r)
+                The orbital frequency
+        """
+        return np.sqrt(self.m_total()/r**3)
+
+    def mass(self, r):
+        """
+        The function returns the total mass enclosed in a sphere of radius r.
+            This includes the central mass and the mass of the dark matter halo if includeHaloInTotalMass=True
+
+        Parameters:
+            r : float or array_like
+                The radius at which to evaluate the mass
+
+        Returns:
+            out : float or array_like (depending on r)
+                The enclosed mass
+        """
+        return np.ones(np.shape(r))*self.m1 + (self.halo.mass(r) if self.includeHaloInTotalMass else 0.)
+    
---
+++ Paper LISA/SMBHBpy/plot_utils.py
@@ -0,0 +1,110 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from itertools import cycle
+from scipy.interpolate import interp1d
+
+from SMBHBpy import halo, inspiral, waveform, constants as c
+
+def plotEvolution(sp, ev, ax_a=None, ax_e=None, label="", ax_ae=None, ax_n=None, color=None, linestyle=None):
+    """
+    Plots the evolution of the system in the natural units that are used throughout the code.
+    The evolution can be plotted as semimajor axis / time, eccentricity / time, eccentricity / semimajor axis,
+        depending on which plt.axes objects are passed.
+    The lines will have the same color and label
+
+    Parameters:
+        sp (merger_system.SystemProp)   : The object describing the properties of the inspiralling system
+        ev (inspiral.Classic.Evolution) : The evolution object that results from the inspiral modeling
+        ax_a (plt.axes)     (optional)  : The axes on which to plot semimajor axis / time
+        ax_e (plt.axes)     (optional)  : The axes on which to plot eccentricity / time
+        ax_ae (plt.axes)    (optional)  : The axes on which to plot eccentricity / semimajor axis
+        ax_n (plt.axes)     (optional)  : The axes on which to plot the braking index / frequency
+        label (string)      (optional)  : The label corresponding to the lines
+        **kwargs                        : Other parameters that can be passed to the plotting
+
+    Returns:
+        out : matplotlib.lines.Line2D
+            The last line object plotted
+    """
+    if not ax_a is None:
+        l, = ax_a.loglog(ev.t/c.year_to_pc, ev.a/(sp.r_isco_1()+sp.r_isco_2()), label=label, color=color, linestyle=linestyle)
+        color = l.get_c()
+    if not ax_e is None:
+        l, = ax_e.loglog(ev.t/c.year_to_pc, ev.e, color=color, label=label, linestyle=linestyle)
+        color = l.get_c()
+    if not ax_ae is None:
+        l, = ax_ae.plot(ev.a/(sp.r_isco_1()+sp.r_isco_2()), ev.e, color=color, label=label, linestyle=linestyle)
+        color = l.get_c()
+    if not ax_n is None:
+        F, n = waveform.BrakingIndex(sp, ev)
+        l, = ax_n.plot(2*F[2:-2]/c.hz_to_invpc, n[2:-2], color=color, label=label, linestyle=linestyle)
+        color = l.get_c()
+    return l
+
+def plotGWcharacteristicStrain(sp, ev, ax_h, label="", acc=1e-13, harmonics=[2], color=None, **kwargs):
+    """
+    Plots the characteristic strain in its harmonics in the natural units that are used throughout the code.
+    The lines will have the same color and alternating linestyle
+
+    Parameters:
+        sp (merger_system.SystemProp)   : The object describing the properties of the inspiralling system
+        ev (inspiral.Classic.Evolution) : The evolution object that results from the inspiral modeling
+        ax_h (plt.axes)                 : The axes on which to plot characteristic strain / frequency
+        harmonics (list of integers)    : The list of harmonics to be plotted
+        label (string)      (optional)  : The label corresponding to the lines
+        color               (optional)  : The color of the lines. Can be anything that matplotlib accepts as color
+        **kwargs                        : Other parameters that can be passed to the plotting
+    """
+    linecycler = cycle(["-", "--", "-.", ":"])
+    color_cycle = ax_h._get_lines.prop_cycler
+    l = None
+    col = color if not color is None else next(color_cycle)['color']
+    for n in harmonics:
+        wf = waveform.h_n(n, sp, ev, acc=acc)
+        l, = ax_h.loglog(wf[0]/c.hz_to_invpc, 2.*wf[0]*np.abs(wf[1]), linestyle=next(linecycler),
+                                 label=label, color=col, **kwargs)
+
+
+def plotDeltaN(sp_0, ev_0, sp_1, ev_1, ax_dN, ax_di=None, n=2, acc=1e-13, plotFgw5year=False, min_dN=10., **kwargs):
+    """
+    Plots the dephasing of a given harmonic in the natural units that are used throughout the code.
+
+    Parameters:
+        sp_0 (merger_system.SystemProp)   : The object describing the properties of the baseline inspiralling system
+        ev_0 (inspiral.Classic.Evolution) : The evolution object that results from the baseline inspiral modeling
+        sp_1 (merger_system.SystemProp)   : The object describing the properties of the alternative inspiralling system
+        ev_1 (inspiral.Classic.Evolution) : The evolution object that results from the alternative inspiral modeling
+        ax_dN (plt.axes)                  : The axes on which to plot the difference in cycles left to observe
+        ax_di (plt.axes)    (optional)    : The axes on which to plot the dephasing index
+        n (int)             (optional)    : The harmonic to be plotted
+        plotFgw5year (bool) (optional)    : Whether to plot the line that represents 5 years to merger in vacuum case
+        min_dN (float)      (optional)    : The minimum dephasing amount for which to plot the dephasing index
+        **kwargs                          : Other parameters that can be passed to the plotting, i.e. label, color, linestyle
+
+    Returns:
+        f_gw1 : array_like
+            The frequencies corresponding to the difference in cycles
+        dN   : array_like
+            The difference in cycles of the two systems
+    """
+    f_gw0, N_0 = waveform.N_cycles_n(n, sp_0, ev_0, acc=acc)
+    N_0interp = interp1d(f_gw0, N_0, kind='cubic', bounds_error=False, fill_value=(0.,0.))
+    f_gw1, N_1 = waveform.N_cycles_n(n, sp_1, ev_1, acc=acc)
+
+    dN = N_1 - N_0interp(f_gw1)
+    l, = ax_dN.loglog(f_gw1/c.hz_to_invpc, np.abs(dN), **kwargs)
+
+    if plotFgw5year:
+        f_gw5yr = interp1d(ev_1.t, f_gw1, kind='cubic', bounds_error=True)(ev_1.t[-1] - 5.*c.year_to_pc)
+        ax_dN.axvline(f_gw5yr/c.hz_to_invpc, linestyle='--', color=l.get_c())
+
+    if not ax_di is None:
+        ddN_df = np.gradient(dN, f_gw1)
+        stop = np.where(np.abs(dN) < min_dN)[0]
+        stop = stop[0] if len(stop) > 0 else len(ddN_df)
+        if 'color' in kwargs:
+            del kwargs['color']
+        ax_di.plot(f_gw1[:stop]/c.hz_to_invpc, (ddN_df/dN * f_gw1)[:stop], color=l.get_c(), **kwargs)
+
+    return f_gw1, dN
